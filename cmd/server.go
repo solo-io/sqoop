c7dc4b863f1fef6dac078f3b40c0addc7c7f9507
Scott Weiss <sdw35@cornell.edu>
2018-05-30 18:53:45 -0400

    basics to configure and invoke gloo
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- cmd/server.go
+++ cmd/server.go
@@ -13,6 +13,11 @@ import (
 	"github.com/vektah/gqlgen/example/starwars"
 	"encoding/json"
 	"github.com/pkg/errors"
+	"github.com/solo-io/gloo/pkg/storage"
+	"github.com/solo-io/gloo/pkg/api/types/v1"
+	"html/template"
+	"bytes"
+	"io/ioutil"
 )
 
 var starWarsSchema = test.StarWarsSchema
@@ -88,4 +93,155 @@ func addResolvers(resolvers *dynamic.ResolverMap) {
 		}
 		return json.Marshal(friends)
 	})
+}
+
+type GlooClient struct {
+	gloo           storage.Interface
+	virtualService string
+	role           string
+}
+
+type Route struct {
+	Path         string
+	Destinations []Destination
+}
+
+type Destination struct {
+	UpstreamName, FunctionName string
+	Weight                     uint32
+}
+
+func (client *GlooClient) SyncVirtualService(resolverRoutes []Route) error {
+	desiredVirtualService, err := client.desiredVirtualService(resolverRoutes)
+	if err != nil {
+		return errors.Wrap(err, "invalid resolver routes")
+	}
+	existingVirtualService, err := client.gloo.V1().VirtualServices().Get(client.virtualService)
+	if err != nil {
+		_, err := client.gloo.V1().VirtualServices().Create(desiredVirtualService)
+		return err
+	}
+	if routesEqual(existingVirtualService.Routes, desiredVirtualService.Routes) {
+		return nil
+	}
+	desiredVirtualService.Metadata.ResourceVersion = existingVirtualService.Metadata.ResourceVersion
+	_, err = client.gloo.V1().VirtualServices().Update(desiredVirtualService)
+	return err
+}
+
+func routesEqual(list1, list2 []*v1.Route) bool {
+	if len(list1) != len(list2) {
+		return false
+	}
+	for i := range list1 {
+		r1, r2 := list1[i], list2[i]
+		if !r1.Equal(r2) {
+			return false
+		}
+	}
+	return true
+}
+
+func (client *GlooClient) desiredVirtualService(resolverRoutes []Route) (*v1.VirtualService, error) {
+	var routes []*v1.Route
+	for _, rr := range resolverRoutes {
+		route, err := resolverRoute(rr)
+		if err != nil {
+			return nil, errors.Wrap(err, "creating route for resolver")
+		}
+		routes = append(routes, route)
+	}
+	return &v1.VirtualService{
+		Name:     client.virtualService,
+		Domains:  []string{"*"},
+		Routes:   routes,
+		Roles:    []string{client.role},
+		Metadata: &v1.Metadata{},
+	}, nil
+}
+
+func resolverRoute(route Route) (*v1.Route, error) {
+	if len(route.Destinations) == 0 {
+		return nil, errors.Errorf("need at least 1 destination to create a resolver route")
+	}
+
+	var (
+		singleDestination *v1.Destination
+		multiDestination  []*v1.WeightedDestination
+	)
+	destinations := route.Destinations
+
+	switch {
+	case len(destinations) == 1:
+		singleDestination = &v1.Destination{
+			DestinationType: &v1.Destination_Function{
+				Function: &v1.FunctionDestination{
+					UpstreamName: destinations[0].UpstreamName,
+					FunctionName: destinations[0].FunctionName,
+				},
+			},
+		}
+	case len(destinations) > 1:
+		for _, dest := range destinations {
+			multiDestination = append(multiDestination, &v1.WeightedDestination{
+				Destination: &v1.Destination{
+					DestinationType: &v1.Destination_Function{
+						Function: &v1.FunctionDestination{
+							UpstreamName: dest.UpstreamName,
+							FunctionName: dest.FunctionName,
+						},
+					},
+				},
+				Weight: dest.Weight,
+			})
+		}
+	}
+
+	return &v1.Route{
+		Matcher: &v1.Route_RequestMatcher{
+			RequestMatcher: &v1.RequestMatcher{
+				Path: &v1.RequestMatcher_PathExact{
+					PathExact: route.Path,
+				},
+				Verbs: []string{"POST"},
+			},
+		},
+		MultipleDestinations: multiDestination,
+		SingleDestination:    singleDestination,
+	}, nil
+}
+
+type GlooResolverFactory struct {
+	ProxyAddr    string
+}
+
+func (gr *GlooResolverFactory) Resolver(path, bodyTemplate, contentType string) (dynamic.RawResolver, error) {
+	if contentType == "" {
+		contentType = "application/json"
+	}
+	tmpl, err := template.New("requestBody").Parse(bodyTemplate)
+	if err != nil {
+		return nil, errors.Wrap(err, "parsing body template failed")
+	}
+	return func(params dynamic.Params) ([]byte, error) {
+		body := &bytes.Buffer{}
+		if err := tmpl.Execute(body, params); err != nil {
+			// TODO: sanitize
+			return nil, errors.Wrapf(err, "executing template for params %v", params)
+		}
+		url := "http://"+gr.ProxyAddr+path
+		res, err := http.Post(url, contentType, body)
+		if err != nil {
+			return nil, errors.Wrap(err, "performing http post")
+		}
+		if res.StatusCode < 200 || res.StatusCode >= 300 {
+			return nil, errors.Errorf("unexpected status code: %v", res.StatusCode)
+		}
+		// empty response
+		if res.Body == nil {
+			return nil, nil
+		}
+		defer res.Body.Close()
+		return ioutil.ReadAll(res.Body)
+	}, nil
 }
