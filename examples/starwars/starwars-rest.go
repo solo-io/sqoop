13b22d1a84890ba4fc55b4b05128eb7c1cf995d8
Scott Weiss <sdw35@cornell.edu>
2018-06-03 21:29:02 -0400

    fix up example
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- examples/starwars/starwars-rest.go
+++ examples/starwars/starwars-rest.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
-	"github.com/vektah/gqlgen/example/starwars"
+	"github.com/solo-io/qloo/examples/starwars/imported/starwars"
 )
 
 var baseResolvers = starwars.NewResolver()
@@ -14,10 +14,10 @@ var baseResolvers = starwars.NewResolver()
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/hero", GetHero).Methods("GET")
-	router.HandleFunc("/api/humans/{id}", GetHuman).Methods("GET")
+	router.HandleFunc("/api/character/{id}", GetCharacter).Methods("GET")
 	// needs to be POST because there's a body
-	router.HandleFunc("/api/humans/{id}/friends", GetHumanFriends).Methods("POST")
-	router.HandleFunc("/api/droids/{id}/friends", GetDroidFriends).Methods("POST")
+	router.HandleFunc("/api/characters", GetCharacters).Methods("POST")
+
 	log.Printf("listening on :9000")
 	log.Fatal(http.ListenAndServe(":9000", router))
 }
@@ -34,52 +34,39 @@ func GetHero(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func GetHuman(w http.ResponseWriter, r *http.Request) {
+func GetCharacter(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
-	friends, err := baseResolvers.Query_human(r.Context(), params["id"])
+	friends, err := baseResolvers.Query_character(r.Context(), params["id"])
 	if err != nil {
 		log.Printf("request failed: %v", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
+	log.Printf("%v", friends)
 	if err := json.NewEncoder(w).Encode(friends); err != nil {
 		log.Printf("encoding failed: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
-func GetHumanFriends(w http.ResponseWriter, r *http.Request) {
-	var input starwars.Human
+func GetCharacters(w http.ResponseWriter, r *http.Request) {
+	var input []string
 	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
 		log.Printf("decoding json: %v", err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	params := mux.Vars(r)
-	input.ID = params["id"]
-	friends, err := baseResolvers.Human_friends(r.Context(), &input)
-	if err != nil {
-		log.Printf("request failed: %v", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	var characters []starwars.Character
+	for _, id := range input {
+		char, err := baseResolvers.Query_character(r.Context(), id)
+		if err != nil {
+			log.Printf("request failed: %v", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		characters = append(characters, char)
 	}
-	if err := json.NewEncoder(w).Encode(friends); err != nil {
-		log.Printf("encoding failed: %v", err)
-	}
-}
-
-func GetDroidFriends(w http.ResponseWriter, r *http.Request) {
-	var input starwars.Droid
-	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
-		log.Printf("decoding json: %v", err)
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	params := mux.Vars(r)
-	input.ID = params["id"]
-	friends, err := baseResolvers.Droid_friends(r.Context(), &input)
-	if err != nil {
-		log.Printf("request failed: %v", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-	if err := json.NewEncoder(w).Encode(friends); err != nil {
+	if err := json.NewEncoder(w).Encode(characters); err != nil {
 		log.Printf("encoding failed: %v", err)
 	}
 }
