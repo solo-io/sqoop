8f55077606049b12d54dbbd1e924958f845bf83b
Scott Weiss <sdw35@cornell.edu>
2018-06-05 13:11:48 -0400

    fill in stub
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- pkg/resolvers/node/node_resolvers.go
+++ pkg/resolvers/node/node_resolvers.go
@@ -2,8 +2,10 @@ package node
 
 import (
 	"github.com/solo-io/qloo/pkg/api/types/v1"
+	"github.com/solo-io/qloo/pkg/exec"
+	"github.com/pkg/errors"
 )
 
-func NewNodeResolver(resolver *v1.NodeJSResolver) {
-
+func NewNodeResolver(resolver *v1.NodeJSResolver) (exec.RawResolver, error) {
+	return nil, errors.Errorf("nodejs resolvers currently unsupported")
 }
