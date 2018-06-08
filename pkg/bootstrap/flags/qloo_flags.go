93e567925fa6114f0844582f5379465e1bf1eeda
Scott Weiss <sdw35@cornell.edu>
2018-06-07 21:47:45 -0400

    kube install, change role name
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- pkg/bootstrap/flags/qloo_flags.go
+++ pkg/bootstrap/flags/qloo_flags.go
@@ -9,7 +9,7 @@ func AddQLooFlags(cmd *cobra.Command, opts *bootstrap.Options) {
 	// TODO ingress.bind-adress
 	cmd.PersistentFlags().StringVar(&opts.VirtualServiceName, "qloo.virtualservice", "qloo-routes", "the " +
 		"name of the virtual service QLoo will use to store its routes")
-	cmd.PersistentFlags().StringVar(&opts.RoleName, "qloo.role", "qloo-role", "the " +
+	cmd.PersistentFlags().StringVar(&opts.RoleName, "qloo.role", "qloo", "the " +
 		"name of the mesh role to assign to QLoo when communicating with Gloo")
 	cmd.PersistentFlags().StringVar(&opts.ProxyAddr, "qloo.proxy-addr", "localhost:8080", "the " +
 		"address (hostname:port) of the QLoo proxy")
