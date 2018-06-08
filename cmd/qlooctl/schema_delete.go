dfd5a5ff3d02100f99d3ee6e4b51d7c3e1eef35e
Scott Weiss <sdw35@cornell.edu>
2018-06-08 17:27:29 -0400

    get and delete
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- cmd/qlooctl/schema_delete.go
+++ cmd/qlooctl/schema_delete.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"github.com/spf13/cobra"
-	"github.com/solo-io/qloo/pkg/api/types/v1"
+	"fmt"
 )
 
-var schemaGetCmd = &cobra.Command{
-	Use:   "get [NAME]",
+var schemaDeleteCmd = &cobra.Command{
+	Use:   "delete [NAME]",
 	Short: "return a schema by its name or list all schemas",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if len(args) != 1 || args[0] == "" {
@@ -21,30 +21,22 @@ var schemaGetCmd = &cobra.Command{
 			}
 			return nil
 		}
-		msg, err := getSchema(args[0])
+		err := deleteSchema(args[0])
 		if err != nil {
 			return err
 		}
-		return printAsYaml(msg)
+		fmt.Println("delete succesful")
 	},
 }
 
 func init() {
-	schemaCmd.AddCommand(schemaGetCmd)
+	schemaCmd.AddCommand(schemaDeleteCmd)
 }
 
-func getSchema(name string) (*v1.Schema, error) {
+func deleteSchema(name string) error {
 	cli, err := makeClient()
 	if err != nil {
-		return nil, err
+		return err
 	}
-	return cli.V1().Schemas().Get(name)
-}
-
-func listSchemas() ([]*v1.Schema, error) {
-	cli, err := makeClient()
-	if err != nil {
-		return nil, err
-	}
-	return cli.V1().Schemas().List()
+	return cli.V1().Schemas().Delete(name)
 }
