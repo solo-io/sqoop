bb5ecd75797db57bce3e3e10e7420dc403413c1f
Scott Weiss <sdw35@cornell.edu>
2018-06-08 17:44:46 -0400

    create and update
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

---
+++ cmd/qlooctl/resolvermap_create.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/solo-io/qloo/pkg/api/types/v1"
+	"github.com/pkg/errors"
+	"io/ioutil"
+	"fmt"
+	"github.com/solo-io/qloo/pkg/storage/file"
+)
+
+var resolverMapCreateOpts struct {
+	FromFile       string
+}
+
+var resolverMapCreateCmd = &cobra.Command{
+	Use:   "create NAME --from-file <path/to/your/qloo/resolver map>",
+	Short: "upload a resolver map to QLoo from a local QLoo ResolverMap yaml file",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 1 {
+			return errors.Errorf("requires exactly 1 argument")
+		}
+		if err := createResolverMap(args[0], resolverMapCreateOpts.FromFile); err != nil {
+			return err
+		}
+		fmt.Println("resolver map created successfully")
+		return nil
+	},
+}
+
+func init() {
+	resolverMapCreateCmd.PersistentFlags().StringVarP(&resolverMapCreateOpts.FromFile, "from-file", "f", "", "path to a "+
+		"graphql resolver map file from which to create the QLoo resolver map object")
+	resolverMapCmd.AddCommand(resolverMapCreateCmd)
+}
+
+func createResolverMap(name, filename string) error {
+	if name == "" {
+		return errors.Errorf("resolver map name must be set")
+	}
+	if filename == "" {
+		return errors.Errorf("filename must be set")
+	}
+	cli, err := makeClient()
+	if err != nil {
+		return err
+	}
+	var resolverMap v1.ResolverMap
+	if err := file.ReadFileInto(filename, &resolverMap); err != nil {
+		return err
+	}
+	_, err = cli.V1().ResolverMaps().Create(&resolverMap)
+	return err
+}
---
+++ cmd/qlooctl/resolvermap_update.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/solo-io/qloo/pkg/api/types/v1"
+	"github.com/pkg/errors"
+	"io/ioutil"
+	"fmt"
+	"github.com/solo-io/qloo/pkg/storage/file"
+)
+
+var resolverMapUpdateOpts struct {
+	FromFile       string
+}
+
+var resolverMapUpdateCmd = &cobra.Command{
+	Use:   "update NAME --from-file <path/to/your/qloo/resolver map>",
+	Short: "upload a resolver map to QLoo from a local QLoo ResolverMap yaml file",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 1 {
+			return errors.Errorf("requires exactly 1 argument")
+		}
+		if err := updateResolverMap(args[0], resolverMapUpdateOpts.FromFile); err != nil {
+			return err
+		}
+		fmt.Println("resolver map updated successfully")
+		return nil
+	},
+}
+
+func init() {
+	resolverMapUpdateCmd.PersistentFlags().StringVarP(&resolverMapUpdateOpts.FromFile, "from-file", "f", "", "path to a "+
+		"graphql resolver map file from which to update the QLoo resolver map object")
+	resolverMapCmd.AddCommand(resolverMapUpdateCmd)
+}
+
+func updateResolverMap(name, filename string) error {
+	cli, err := makeClient()
+	if err != nil {
+		return err
+	}
+	if name == "" {
+		return errors.Errorf("schema name must be set")
+	}
+	if filename == "" {
+		return errors.Errorf("filename must be set")
+	}
+
+	existing, err := cli.V1().ResolverMaps().Get(name)
+	if err != nil {
+		return err
+	}
+
+	if name == "" {
+		return errors.Errorf("resolver map name must be set")
+	}
+	if filename == "" {
+		return errors.Errorf("filename must be set")
+	}
+	var resolverMap v1.ResolverMap
+	if err := file.ReadFileInto(filename, &resolverMap); err != nil {
+		return err
+	}
+	resolverMap.Metadata = existing.Metadata
+	_, err = cli.V1().ResolverMaps().Update(&resolverMap)
+	return err
+}
