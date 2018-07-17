99fec0e26cf8e7e4cc5e9f71d8cc74a7164cfd2e
Scott Weiss <sdw35@cornell.edu>
2018-07-17 14:38:52 -0400

    bump release version and gloo versions
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- hack/upload-github-release-asset.sh
+++ hack/upload-github-release-asset.sh
@@ -17,7 +17,7 @@
 #
 # Example:
 #
-# upload-github-release-asset.sh github_api_token=TOKEN owner=stefanbuck repo=playground tag=v0.1.0 filename=./build.zip
+# upload-github-release-asset.sh github_api_token=TOKEN owner=stefanbuck repo=playground tag=v0.1.1 filename=./build.zip
 #
 
 # Check dependencies.
